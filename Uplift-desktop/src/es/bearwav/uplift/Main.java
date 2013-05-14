1244af0603dfe1c5436090dee87402add56fdcc2
JoelOtter <joel.auterson@googlemail.com>
2013-05-14 22:59:52 +0100

    First commit

---
+++ Uplift-desktop/src/es/bearwav/uplift/Main.java
@@ -0,0 +1,16 @@
+package es.bearwav.uplift;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Uplift";
+		cfg.useGL20 = true;
+		cfg.width = 800;
+		cfg.height = 480;
+		
+		new LwjglApplication(new GdxGame(), cfg);
+	}
+}
