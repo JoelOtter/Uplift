8b6e85ab1ad7a7d5b46be1e53d33af2f13d520ef
JoelOtter <joel.auterson@googlemail.com>
2013-05-28 16:17:55 +0100

    Added player animation, though it doesn't work for diagonals yet.

--- Uplift/src/es/bearwav/uplift/entity/Player.java
+++ Uplift/src/es/bearwav/uplift/entity/Player.java
@@ -3,8 +3,10 @@ package es.bearwav.uplift.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
 import es.bearwav.uplift.Input;
 import es.bearwav.uplift.screen.Screen;
@@ -16,10 +18,20 @@ public class Player extends Entity {
 	private TextureRegion up;
 	private TextureRegion left;
 	private TextureRegion right;
+	private Array<TextureRegion> leftFrames;
+	private Array<TextureRegion> rightFrames;
+	private Array<TextureRegion> upFrames;
+	private Array<TextureRegion> downFrames;
 	private Vector2 velocity;
 	private int direction;
 	private TextureRegion currentFrame;
 	private static final float MAX_SPEED = 1.5f;
+	private float stateTime;
+	private float animSpeed;
+	private Animation rightAnimation;
+	private Animation leftAnimation;
+	private Animation upAnimation;
+	private Animation downAnimation;
 
 	public Player(float x, float y) {
 		super(x, y);
@@ -32,12 +44,29 @@ public class Player extends Entity {
 		up = tmp[0][1];
 		right = tmp[1][1];
 		left = tmp[2][1];
+		animSpeed = 0.25f;
+		leftFrames = new Array<TextureRegion>();
+		upFrames = new Array<TextureRegion>();
+		downFrames = new Array<TextureRegion>();
+		rightFrames = new Array<TextureRegion>();
+		for (int i = 0; i < 4; i++){
+			upFrames.add(tmp[0][i]);
+			rightFrames.add(tmp[1][i]);
+			leftFrames.add(tmp[2][i]);
+			downFrames.add(tmp[3][i]);
+		}
+		rightAnimation = new Animation(animSpeed, rightFrames, Animation.LOOP);
+		leftAnimation = new Animation(animSpeed, leftFrames, Animation.LOOP);
+		upAnimation = new Animation(animSpeed, upFrames, Animation.LOOP);
+		downAnimation = new Animation(animSpeed, downFrames, Animation.LOOP);
 		direction = 2;
+		stateTime = 0;
 		currentFrame = down;
 	}
 
 	@Override
 	public void render(Screen screen, Camera cam) {
+		stateTime += Gdx.graphics.getDeltaTime();
 		screen.draw(currentFrame, x, y);
 	}
 
@@ -81,25 +110,27 @@ public class Player extends Entity {
 	}
 
 	private void walk(int dir) {
+		if (dir != direction) stateTime = 0;
+		
 		if (dir == 0) velocity.y = MAX_SPEED;
 		if (dir == 1) velocity.x = MAX_SPEED;
 		if (dir == 2) velocity.y = -MAX_SPEED;
 		if (dir == 3) velocity.x = -MAX_SPEED;
 		
 		if (velocity.y > 0){
-			currentFrame = up;
+			currentFrame = upAnimation.getKeyFrame(stateTime);
 			direction = 0;
 		}
 		else if (velocity.y < 0){
-			currentFrame = down;
+			currentFrame = downAnimation.getKeyFrame(stateTime);
 			direction = 2;
 		}
 		else if (velocity.x > 0){
-			currentFrame = right;
+			currentFrame = rightAnimation.getKeyFrame(stateTime);
 			direction = 1;
 		}
 		else if (velocity.x < 0){
-			currentFrame = left;
+			currentFrame = leftAnimation.getKeyFrame(stateTime);
 			direction = 3;
 		}
 	}
