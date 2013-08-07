ee4c41cdfe800b4e39a17722c0afd733ad26eb11
JoelOtter <joel.auterson@googlemail.com>
2013-08-07 19:32:27 +0100

    Enemies added, player now takes damage though there's no kickback, red flash or combat yet.

---
+++ Uplift/src/es/bearwav/uplift/entity/Enemy.java
@@ -0,0 +1,165 @@
+package es.bearwav.uplift.entity;
+
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.utils.Array;
+
+import es.bearwav.uplift.Input;
+import es.bearwav.uplift.level.Level;
+import es.bearwav.uplift.screen.Screen;
+
+public class Enemy extends Entity{
+	
+	private String type;
+	private String tileset;
+	private float hp;
+	private Texture enemyTex;
+	private Array<TextureRegion> leftFrames;
+	private Array<TextureRegion> rightFrames;
+	private Array<TextureRegion> upFrames;
+	private Array<TextureRegion> downFrames;
+	private Vector2 velocity;
+	private int direction;
+	private TextureRegion currentFrame;
+	private static final float MAX_SPEED = 70f;
+	private float stateTime;
+	private float directionTime;
+	private float changeRate;
+	private float animSpeed;
+	private Animation rightAnimation;
+	private Animation leftAnimation;
+	private Animation upAnimation;
+	private Animation downAnimation;
+	private Body body;
+	private static final float enemyScale = 0.6f;
+	private boolean attacking = false;
+	private Random rand = new Random();
+	private boolean collidable = true;
+
+	public Enemy(Array<?> data, Level l) {
+		super((Float) data.get(2), (Float) data.get(3), l);
+		type = (String) data.get(0);
+		tileset = (String) data.get(1);
+		hp = (Float) data.get(4);
+		enemyTex = new Texture(Gdx.files.internal("gfx/" + tileset));
+		this.w = enemyTex.getWidth() / 4;
+		this.h = enemyTex.getHeight() / 5;
+		TextureRegion[][] tmp = TextureRegion.split(enemyTex, w, h);
+		leftFrames = new Array<TextureRegion>();
+		upFrames = new Array<TextureRegion>();
+		downFrames = new Array<TextureRegion>();
+		rightFrames = new Array<TextureRegion>();
+		for (int i = 0; i < 4; i++) {
+			upFrames.add(tmp[1][i]);
+			rightFrames.add(tmp[2][i]);
+			leftFrames.add(tmp[3][i]);
+			downFrames.add(tmp[4][i]);
+		}
+		animSpeed = 0.2f;
+		rightAnimation = new Animation(animSpeed, rightFrames, Animation.LOOP);
+		leftAnimation = new Animation(animSpeed, leftFrames, Animation.LOOP);
+		upAnimation = new Animation(animSpeed, upFrames, Animation.LOOP);
+		downAnimation = new Animation(animSpeed, downFrames, Animation.LOOP);
+		direction = 1;
+		stateTime = 0;
+		directionTime = 0;
+		changeRate = 3;
+		currentFrame = leftFrames.get(1);
+		
+		//Physics
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(x + (w * enemyScale)/2, y + (h * enemyScale)/2);
+		body = l.world.createBody(bodyDef);
+		PolygonShape groundBox = new PolygonShape();
+		groundBox.setAsBox(w/2 * enemyScale, h/2 * enemyScale);
+		body.createFixture(groundBox, 0.0f).setRestitution(0.5f);
+		groundBox.dispose();
+		body.setLinearVelocity(0, 0);
+		body.setFixedRotation(true);
+		body.setUserData(this);
+		velocity = new Vector2(0, 0);
+	}
+
+	@Override
+	public void render(Screen screen, Camera cam) {
+		float time = Gdx.graphics.getDeltaTime();
+		stateTime += time;
+		directionTime += time;
+		screen.draw(currentFrame, x, y, w * enemyScale, h * enemyScale);
+		x = body.getPosition().x - (w * enemyScale)/2;
+		y = body.getPosition().y - (h * enemyScale)/2;
+	}
+	
+	public void tick(Input input){
+		if (!attacking){
+			if (directionTime > changeRate){
+				directionTime = 0;
+				setDirection(rand.nextInt(4));
+			}
+		}
+		body.setLinearVelocity(velocity);
+		animate(direction);
+	}
+	
+	private void setDirection(int dir){
+		direction = dir;
+		switch (dir){
+			case 0: velocity.x = 0; velocity.y = MAX_SPEED; break;
+			case 1: velocity.x = MAX_SPEED; velocity.y = 0; break;
+			case 2: velocity.x = 0; velocity.y = -MAX_SPEED; break;
+			case 3: velocity.x = -MAX_SPEED; velocity.y = 0; break;
+		}
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void collide(Object collider) {
+		if (collider instanceof Player){
+			((Player) collider).damage(direction);
+		}
+		if (collidable){
+			//Reverse direction
+			setDirection((direction + 2) % 4);
+			collidable = false;
+		}
+	}
+
+	@Override
+	public void endContact(Object collider) {
+		collidable = true;
+	}
+	
+	private void animate(int dir) {
+		switch (dir) {
+		case 0:
+			currentFrame = upAnimation.getKeyFrame(stateTime);
+			break;
+		case 1:
+			currentFrame = rightAnimation.getKeyFrame(stateTime);
+			break;
+		case 2:
+			currentFrame = downAnimation.getKeyFrame(stateTime);
+			break;
+		case 3:
+			currentFrame = leftAnimation.getKeyFrame(stateTime);
+			break;
+		}
+	}
+
+}
